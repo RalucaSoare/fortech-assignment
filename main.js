06839bca98f5fc83342d5b85114f5caadaab3bf9
Raluca Soare <soareralucamaria@gmail.com>
2022-11-13 21:52:54 +0200

    changed return value

--- main.js
+++ main.js
@@ -3,6 +3,6 @@ let btn = document.getElementById('btn');
 btn.addEventListener('click', function () {
   let weight = document.getElementById('weight-input').value;
   let height = document.getElementById('height-input').value;
-  let finalbmi = (weight / (height * height)) * 10000;
+  let finalbmi = ((weight / (height * height)) * 10000).toFixed(2);
   document.getElementById('bmi-output').value = finalbmi;
 });
